c754cd67e8c2bca3a703a68daed313a44adc4567
Andreas Rezki Wibowo Napitupulu <andreasrezki21@gmail.com>
2017-04-07 09:41:04 +0700

    Add files via upload

---
+++ Jurnal.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class Jurnal {
+public static void main (String[]args) {
+    
+        Scanner show = new Scanner(System.in);
+        System.out.print("Masukan Jumlah Deret Fibonacci ");
+        int n = show.nextInt();
+        long fib[] = new long[n];
+         
+        int i=1;
+        fib[0] = 0;
+        fib[1] = 1;
+         
+        for(i = 2; i < n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+         
+        for (i = 0; i < n; i++) {
+            System.out.print(fib[i] +  " ");
+        }
+    }
+ 
+}
